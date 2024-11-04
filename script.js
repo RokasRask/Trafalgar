bfaf53eea68f4bab7fe9672906f9365ff8386fc4
Aftesmo <r0km3n45@gmail.com>
2024-11-04 20:41:41 +0200

    finish

---
+++ script.js
@@ -0,0 +1,45 @@
+
+const slides = document.querySelectorAll(".slide");
+const radio = document.querySelectorAll('.slide input');
+let radioIndex = 0;
+let slideIndex = 0;
+let intervalId = null;
+
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+function initializeSlider(){
+    if(slides.length > 0){
+        slides[slideIndex].classList.add("displaySlide");
+        intervalId = setInterval(nextSlide, 5000);
+    }
+}
+
+function showSlide(index){
+    if(index >= slides.length){
+        slideIndex = 0;
+    }
+    else if(index < 0){
+        slideIndex = slides.length - 1;
+    }
+
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    slides[slideIndex].classList.add("displaySlide");
+
+    const radioButtons = document.querySelectorAll('.buttons input[type="radio"]');
+    radioButtons.forEach((radio, i) => {
+        radio.checked = (i === slideIndex);
+    });
+}
+
+function prevSlide(){
+    clearInterval(intervalId);
+    slideIndex--;
+    showSlide(slideIndex);
+}
+
+function nextSlide(){
+    slideIndex++;
+    showSlide(slideIndex);
+}
